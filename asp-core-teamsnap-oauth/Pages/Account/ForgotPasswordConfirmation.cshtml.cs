2a63297ce0149e4ae6ec07f57a3999572688417b
Ahmed Mohamed <egytech2013@hotmail.com>
2018-04-14 20:43:25 +0200

    Init ASP.NET Core Project

---
+++ asp-core-teamsnap-oauth/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_core_teamsnap_oauth.Pages.Account
+{
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
